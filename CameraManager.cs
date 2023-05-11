c8201ecb381ebcfa1375ca6c50e090e7b6d30863
Halmai Ákos <Halmai kos@DESKTOP-BT26SDG>
2023-05-11 21:48:01 +0200

    Startups & Shutdown

---
+++ CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace VmbNET
+{
+    public static class CameraManager
+    {
+        #region Private Constants
+        private const string dllName = @"C:\Program Files\Allied Vision\Vimba X\api\bin\VmbC.dll";
+        #endregion
+
+        #region Error Handling
+        [MethodImpl(MethodImplOptions.AggressiveInlining), SkipLocalsInit]
+        private static void DetectError(ErrorType errorType)
+        {
+            if (errorType is not ErrorType.VmbErrorSuccess)
+                ThrowAPIError(errorType);
+
+            [DoesNotReturn]
+            static void ThrowAPIError(ErrorType errorType) => throw new Exception(errorType switch
+            {
+                ErrorType.VmbErrorInternalFault => "Internal fault.",
+                ErrorType.VmbErrorSuccess => "No error."
+            });
+        }
+        #endregion
+    }
+}
---
+++ ErrorType.cs
@@ -0,0 +1,8 @@
+﻿namespace VmbNET
+{
+    public enum ErrorType
+    {
+        VmbErrorSuccess = 0,
+        VmbErrorInternalFault = -1
+    }
+}
